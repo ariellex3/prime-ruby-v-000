efbd090bc53961b0b624c728cbe4cbaaac2e3cef
Arielle D <domingu4@gmail.com>
2018-09-28 19:47:44 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime?(number)
   if number < 0 or number == 0 or number == 1
     return false
   else
-    (2..number-1).to_a do |factor|
+    (2..number-1).to_a.all? do |factor|
       number % factor != 0
     end
   end
